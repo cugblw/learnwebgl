e614d84eea86b4f93ee69e83f2dcd7b93ff6b245
Lee <cugblw2014@outlook.com>
2022-09-16 08:29:58 +0800

    鼠标点击绘制点

--- exercises/ClickedPoints.js
+++ exercises/ClickedPoints.js
@@ -9,9 +9,7 @@ var VSHADER_SOURCE =
 
 // 片元着色器
 var FSHADER_SOURCE =
-  "void main() {\n" +
-  "  gl_FragColor = vec4(0.0, 1.0, 0.0, 1.0);\n" + // 设置颜色
-  "}\n";
+  "void main() {\n" + "  gl_FragColor = vec4(1.0, 0.0, 0.0, 1.0);\n" + "}\n";
 
 function main() {
   // 获取<canvas>元素
@@ -38,35 +36,45 @@ function main() {
   }
 
   // 注册鼠标点击事件响应函数
-  canvas.onmousedown = function (ev) { click(ev, gl, canvas, a_Position); };
+  canvas.onmousedown = function (ev) {
+    click(ev, gl, canvas, a_Position);
+  };
 
   // 将顶点位置传输给attribute变量
-  gl.vertexAttrib3f(a_Position, 0.5, 0.0, 0.0);
+  gl.clearColor(0.0, 0.0, 0.0, 1.0);
 
   // 设置<canvas>的背景色
-  gl.clearColor(gl.COLOR_BUFFER_BIT);
+  gl.clear(gl.COLOR_BUFFER_BIT);
 }
 
 var g_points = []; // 鼠标点击位置数组
-function click(ev, gl, canvas, a_Position) { 
-    var x = ev.clientX; // 鼠标点击处的x坐标
-    var y = ev.clientY; // 鼠标点击处的y坐标
-    var rect = ev.target.getBoundingClientRect();
+function click(ev, gl, canvas, a_Position) {
+  var x = ev.clientX; // 鼠标点击处的x坐标
+  var y = ev.clientY; // 鼠标点击处的y坐标
+  var rect = ev.target.getBoundingClientRect();
 
-    x = ((x - rect.left) - canvas.width / 2) / (canvas.width / 2);
-    y = (canvas.height / 2 - (y - rect.top)) / (canvas.height / 2);
-    // 将坐标存储到g_points数组中
-    g_points.push(x); g_points.push(y);
+  x = (x - rect.left - canvas.width / 2) / (canvas.width / 2);
+  y = (canvas.height / 2 - (y - rect.top)) / (canvas.height / 2);
+  // 将坐标存储到g_points数组中
+  // g_points.push(x); g_points.push(y);
+  g_points.push([x, y]);
 
-    // 清空<canvas>
-    gl.clear(gl.COLOR_BUFFER_BIT);
+  // 清空<canvas>
+  gl.clear(gl.COLOR_BUFFER_BIT);
 
-    var len = g_points.length;
-    for (var i = 0; i < len; i += 2) {
-        // 将点的位置传输到attribute变量
-        gl.vertexAttrib3f(a_Position, g_points[i], g_points[i + 1], 0.0);
+  var len = g_points.length;
+  // for (var i = 0; i < len; i += 2) {
+  //     // 将点的位置传输到attribute变量
+  //     gl.vertexAttrib3f(a_Position, g_points[i], g_points[i + 1], 0.0);
 
-        // 绘制点
-        gl.drawArrays(gl.POINTS, 0, 1);
-    }
+  //     // 绘制点
+  //     gl.drawArrays(gl.POINTS, 0, 1);
+  // }
+  for (var i = 0; i < len; i++) {
+    var xy = g_points[i];
+    // 将点的位置传输到attribute变量
+    gl.vertexAttrib3f(a_Position, xy[0], xy[1], 0.0);
+    // 绘制点
+    gl.drawArrays(gl.POINTS, 0, 1);
+  }
 }
